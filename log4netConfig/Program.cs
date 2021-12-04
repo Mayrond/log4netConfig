893163362377b4486dc877d582486431e6596fc7
Vagif <mekhtiev@skbkontur.ru>
2021-12-04 12:24:12 +0300

    Default template project

---
+++ log4netConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace log4netConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
