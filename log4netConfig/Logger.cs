52c0229943d262d683d4378d6a807ebd9eaa9f4d
Vagif <mayrond@mail.ru>
2021-12-04 15:03:38 +0300

    Конфигурация через BasicConfigurator

--- log4netConfig/Logger.cs
+++ log4netConfig/Logger.cs
@@ -1,24 +1,21 @@
-﻿using log4net;
-using log4net.Appender;
+﻿using log4net.Appender;
+using log4net.Config;
 using log4net.Core;
 using log4net.Layout;
-using log4net.Repository.Hierarchy;
 
 namespace log4netConfig
 {
-    public class Logger
+    public static class Logger
     {
         public static void ConfigureLogging()
         {
-            var hierarchy = (Hierarchy)LogManager.GetRepository();
-
             var patternLayout = new PatternLayout
             {
                 ConversionPattern = "%date [%thread] %-5level %logger - %message%newline",
             };
             patternLayout.ActivateOptions(); //apply options
 
-            var roller = new RollingFileAppender
+            var fileAppender = new RollingFileAppender
             {
                 AppendToFile = true,
                 Threshold = Level.Debug,
@@ -28,8 +25,7 @@ namespace log4netConfig
                 RollingStyle = RollingFileAppender.RollingMode.Date,
                 StaticLogFileName = false
             };
-            roller.ActivateOptions(); //apply options
-            hierarchy.Root.AddAppender(roller);
+            fileAppender.ActivateOptions(); //apply options
 
             var consoleAppender = new ConsoleAppender()
             {
@@ -38,13 +34,10 @@ namespace log4netConfig
                 Target = "Console.Out"
             };
             consoleAppender.ActivateOptions(); //apply options
-            hierarchy.Root.AddAppender(consoleAppender);
 
             var customAppender = new CustomAppender();
-            hierarchy.Root.AddAppender(customAppender);
-
-            hierarchy.Root.Level = Level.Debug;
-            hierarchy.Configured = true;
+            
+            BasicConfigurator.Configure(fileAppender, consoleAppender, customAppender);
         }
     }
 }
